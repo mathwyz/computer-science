2a2d2d891c8903638bbc69c1a5a4217274f5610a
wyz <1269626032@qq.com>
2020-06-05 10:10:27 +0800

    新文件：   student.dat 	修改：     test11.c 	修改：     test11.c~ 	新文件：   test11.o 	新文件：   test12.c 	新文件：   test12.c~

--- c/test11.c
+++ c/test11.c
@@ -11,20 +11,65 @@ typedef struct student
   float score[3];
 } STU;
 
+void
+fun (char *filename, STU n)
+{
+  FILE *fp;
+  fp = fopen (filename, "rb+");
+  fseek (fp, -(long) sizeof (STU), SEEK_END);
+  fwrite (&n, sizeof (STU), 1, fp);
+  fclose (fp);
+}
+
+int
 main ()
 {
-  STU t[N]
-  {
+  STU t[N] = {
     {
-    10001, "Machao", 91, 92, 77},
+     10001, "Machao", 91, 92, 77},
     {
-    10002, "CaoKai", 75, 60, 88},
+     10002, "CaoKai", 75, 60, 88},
     {
-    10003, "Lisi", 85, 70, 78},
+     10003, "Lisi", 85, 70, 78},
     {
-    10004, "FangFang", 90, 82, 87},
+     10004, "FangFang", 90, 82, 87},
     {
-  10005, "Zhangsan", 95, 80, 88}};
+     10005, "Zhangsan", 95, 80, 88}
+  };
 
-  SUT n = { }
+  STU n = { 10006, "Zhaosi", 55, 70, 68 }, ss[N];
+  int i, j;
+  FILE *fp;
+
+  fp = fopen ("student.dat", "wb");
+  fwrite (t, sizeof (STU), N, fp);
+  fclose (fp);
+
+  fp = fopen ("student.dat", "rb");
+  fread (ss, sizeof (STU), N, fp);
+  fclose (fp);
+
+  printf ("\nThe original data:\n\n");
+
+  for (j = 0; j < N; j++)
+    {
+      printf ("\nNo:%ld Name :%-8s scores:", ss[j].sno, ss[j].name);
+      for (i < 0; i < 3; i++)
+	printf ("%6.2d", ss[j].name[i]);
+      printf ("\n");
+    }
+
+  fun ("studentf.dat", n);
+
+  printf ("\nThe data after modifing:\n\n");
+
+  fp = fopen ("student.dat", "rb");
+  fread (ss, sizeof (STU), N, fp);
+  fclose (fp);
+
+  for (j = 0; j < N; j++)
+    {
+      printf ("\nNo:%ld Name:%-8s Scores:", ss[j].sno, ss[j].name);
+      printf ("\n");
+    }
 }
