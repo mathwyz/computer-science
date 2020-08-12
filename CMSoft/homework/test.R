0c34aa0e5d910ccec4f87fdf1d778ca917137084
王逸舟 <mathwyz@163.com>
2020-08-12 10:59:56 +0800

    删除：     "SRE\351\203\250\351\227\250\346\232\221\346\234\237\345\256\236\344\271\240\347\224\237\345\237\271\345\205\273\345\267\245\344\275\234\350\256\241\345\210\222-\347\233\221\346\216\247\345\256\242\345\223\215\347\273\204.docx" 	删除：     "SRE\351\203\250\351\227\250\346\232\221\346\234\237\345\256\236\344\271\240\347\224\237\345\237\271\345\205\273\345\267\245\344\275\234\350\256\241\345\210\2220802.docx" 	新文件：   homework/test.R 	新文件：   homework/test.R~ 	删除：     "\351\231\204\344\273\2664\357\274\232\346\232\221\346\234\237\345\256\236\344\271\240\347\224\237\345\267\245\344\275\234\345\255\246\344\271\240\346\210\220\346\236\234\346\261\207\346\212\245\346\250\241\346\235\277.pptx"

---
+++ CMSoft/homework/test.R
@@ -0,0 +1,16 @@
+require("corplot")
+
+
+data1<-read.csv("*.scv")
+data2<-read.csv("*.scv")
+data3<-read.csv("*.scv")
+data4<-read.csv("*.scv")
+data5+
+
+<-read.csv("*.scv")
+
+summary(data)
+
+xtable(summary(data))
+
+##协方差分析
